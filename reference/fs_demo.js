8028b4fb1bd6879d64a001b43dddda24a3d3a8e2
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2021-01-04 10:28:57 +0100

    fs methods

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -16,10 +16,29 @@ fs.writeFile(
     console.log("Filed written to");
 
     // File append
-    fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),
-    'I love Node Js', err=> {
-      if (err) throw err;
-      console.log('File witten to..')
-    })
+    fs.appendFile(
+      path.join(__dirname, "/test", "hello.txt"),
+      "I love Node Js",
+      (err) => {
+        if (err) throw err;
+        console.log("File witten to..");
+      }
+    );
+  }
+);
+
+// Read file
+fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+// Rename File
+fs.rename(
+  path.join(__dirname, "/test", "hello.txt"),
+  path.join(__dirname, "/test", "helloworld.txt"),
+  (err) => {
+    if (err) throw err;
+    console.log('File renamed');
   }
 );
